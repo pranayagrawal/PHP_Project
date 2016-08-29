30ecd3eea8c3b2c80b7419163b39254047b8c838
pranayagrawal <pranayagrawal0000@gmail.com>
2016-08-29 13:57:41 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,28 @@
+<html>
+<div><br/><center><h2><font face="Lucida Handwriting" size="+1" color="#00CCFF">ABOUT US</font></h2></center></div>
+<div style="width:100%;float:left">
+<div>
+  <p><br/>
+  
+  <div align="center"><img src="usepics/fw.png"/></div>
+	  <div>
+	  <p>
+              <br>
+          <p><font color="#009966" size="+1">Fashion Zone is a cool casual-wear brand. Our customers are our focus and we believe in connecting with them to the fullest.</font> </p>
+          <br>
+          <p><font color="#009966" size="+1">We mirror the fondness of today’s youth and intend to sell a unique product line which conforms to latest trend standards at unbeatable prices.</font></p> 
+<br>
+<p><font color="#009966" size="+1">Fashion zone has taken the revolutionary approach to create products in house and sell directly to customers. It’s a no-middleman, no-rentals, no-markups business model, which is centered around selling straight to customers online.</font> </p>
+<br>
+<p><font color="#009966" size="+1">Inspired by design and driven by creativity, our passionate team strives to offer a wide range of up-to-the-minute styles that is ‘quality guaranteed’. Available in vibrant colors, clean neutrals and prints & patterns, our fashion line defines the free spirit of the NexGen..</font></p></p>
+	 <br>
+          </div>
+          
+          
+          
+          
+          
+          
+	  </div>
+	  </div>
+	  </html>
---
+++ home.php
@@ -0,0 +1,57 @@
+<head>
+<script>
+function abc()
+{
+var arr=new Array("usepics/1.png","usepics/clear.png","usepics/google.jpg","usepics/3.png","usepics/women.jpg");
+var ind=eval(document.f1.h1.value);
+document.img.src=arr[ind];
+document.f1.h1.value=ind+1;
+if(document.f1.h1.value==5)
+{
+document.f1.h1.value=0;
+}
+}
+setInterval("abc()",3000);
+</script>
+</head>
+<body>
+<div id="RightPart">
+    <div id="Page"><img src="usepics/clear.png" alt="" width="669" height="210" name="img"/>
+  <form name="f1">
+  <input type="hidden" name="h1" value="0" />
+  </form>
+        <div><br/><center><h2><font face="calibri" size="+2" color="#00CCFF">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Latest Fashion Trends</font></h2></center></div>
+ <table border="0">
+ <tr><td> 
+<img src="usepics/news1.jpg" width="200" height="200"/>
+</td>
+<td colspan="2">
+<font face="calibri" size="+2" color="#99CC33" >
+Corrugated Modernist Christian Dior Couture</font><br>
+<font face="Comic Sans MS"><strong>This petite Goa-based blogger isn't one for soft, pretty imagery. She may be shooting against expansive beaches, blue skies and lush greens but her images carry an unexpected haunting quality, full of depth and dark overtones. She's collaborated with a host of brands, from L'Oreal and Nike to Tommy Hilfiger and Quirk Box. Turn to her blog for beautifully shot photographs infused with a sense of quiet sophistication and retro charm.</strong></font></td>
+</tr>
+<tr>
+<td>
+
+<img src="usepics/news2.jpg" width="200" height="200"/><br>
+<font  color="#99CC33" face="calibri" size="+1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Trends in INDIA</font><br>
+<font face="Comic Sans MS"><strong>This brand consultant uses Republic of Chic as a platform to share with her followers her personal style (sexy, sleek, sophisticated), seasonal trends, beauty tips, Bangalore's local fashion, and collaborations with brands like Swarovski and Karishma Shahani Khan
+</strong></font></td>
+
+<td>
+<img src="usepics/news3.jpg" &nbsp width="200" height="200"/><br>
+<font  color="#99CC33" face="calibri" size="+1">The VOGUE Blogger </font><br>
+
+<font face="Comic Sans MS"><strong>
+Head to Akanksha Redhu's blog for a daily dose of fashion, with a bonus of beauty. Her posts include her personal interpretation of the season's biggest fashion trends (stripes, florals, double denim), coverage of Delhi fashion weeks and other local events, her favourite beauty picks, and new products she’s loving. </strong></font></td>
+<td>
+<img src="usepics/news4.jpg" width="200" height="200"/><br>
+<font  color="#99CC33" face="calibri" size="+1"> Razzedazzlepickle.com </font><br>
+
+<font face="Comic Sans MS"><strong>This blog brings together Ritu Arya's two passions—music and fashion. Also the co-founder of Drum and Bass India, Arya regularly posts images documenting her hipster grunge style (think beanie caps, plaid shirts and lace-up ankle boots) and shares playlists of the music she’s currently obsessing over.adactylous
+</strong></font></td>
+</tr>
+</table>
+  </div>
+  </div>
+  </body>
